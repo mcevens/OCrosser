2f6ed66a602a95e4708a8c2afe4a3ae0fa646b04
Mc Evens <mcevens.bienaime@gmail.com>
2017-07-05 17:34:14 -0400

    Adding water and road to the canvas

---
+++ ocrosser.js
@@ -0,0 +1,36 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+ctx.fillStyle = "lime" ;
+ctx.fillRect(0, 440, 800, 45);
+ctx.fillRect(0, 220, 800, 45);
+
+ctx.beginPath();
+ctx.moveTo(0,395);
+ctx.lineTo(800, 395);
+ctx.strokeStyle = "white";
+ctx.setLineDash([5]);
+ctx.strokeWidth = 2;
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(0,350);
+ctx.lineTo(800,350);
+ctx.strokeStyle= "yellow";
+ctx.setLineDash([0]);
+ctx.strokeWidth = 12;
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(0,305);
+ctx.lineTo(800,305);
+ctx.strokeStyle = "white";
+ctx.setLineDash([5]);
+ctx.strokeWidth = 2;
+ctx.stroke();
+
+
+//drawing water
+
+ctx.fillStyle = "blue";
+ctx.fillRect(0,0,800,220);
