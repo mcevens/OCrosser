a4b30d0d7484b4972eb741f5d85c352e6a4c82a4
Mc Evens <mcevens.bienaime@gmail.com>
2017-07-06 16:44:52 -0400

    Add Crosser image on screen

--- ocrosser.js
+++ ocrosser.js
@@ -1,6 +1,17 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
+let ocrosser = new Image();
+ocrosser.src = "ocrosser.png";
+
+let sx = 0 ;
+let sy = 0 ;
+let swidth = 40;
+let sheight = 40;
+let x = 50;
+let y = 444;
+let width = 30;
+let height = 30;
 
 function drawBackground(){
 
@@ -39,9 +50,13 @@ function drawBackground(){
   ctx.fillRect(0,0,800,220);
 }
 
+function drawOCrosser(){
+  ctx.drawImage(ocrosser, sx, sy, swidth, sheight, x, y, width, height);
+}
+
 function draw(){
   drawBackground();
-
+  drawOCrosser();
   requestAnimationFrame(draw);
 }
 
